1b62f747bdff61b550fcd038ae212d6f53c1927e
RakeshG <rockingrakeshshetty@gmail.com>
2023-07-04 17:40:11 +0530

    user model added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 import express, { json, urlencoded } from "express";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
+
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.listen(5000, () => console.log("server started"));
